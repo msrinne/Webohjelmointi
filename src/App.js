c4e8e41e2d2719b0cca6347765393e37df474665
msrinn <msrinn@utu.fi>
2023-02-13 16:38:58 +0200

    tehtävät 1-1.5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+  const course = { 
+      name: 'Half Stack application development',
+      parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <div>
+        <p>{props.name} {props.exercises}</p>
+      </div>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <div>
+        <Part name={course.parts[0].name} exercises={course.parts[0].exercises}/>
+        <Part name={course.parts[1].name} exercises={course.parts[1].exercises}/>
+        <Part name={course.parts[2].name} exercises={course.parts[2].exercises}/>
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Total {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course}/>
+      <Content parts={course}/>
+      <Total parts={course}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
